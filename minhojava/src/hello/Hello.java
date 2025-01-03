58a685b3779a0af678e85ec6ab44156f50475bf6
rivermh <kmh000904@gmail.com>
2025-01-04 00:23:05 +0900

    20250104

--- minhojava/src/hello/Hello.java
+++ minhojava/src/hello/Hello.java
@@ -1,10 +1,10 @@
 package hello;
 
 public class Hello {
-
+		//프로그램 실행 진입점
 	public static void main(String[] args) {
+		//콘솔레 출력하는 실행문
 		System.out.println("Hello, Java");
-		System.out.println("민호 ㅎㅇ");
 	}
  
 }
