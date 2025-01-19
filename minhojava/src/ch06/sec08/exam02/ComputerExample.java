d7ed07535a7be2c3c9600615693391d07ac0d11b
rivermh <kmh000904@gmail.com>
2025-01-19 19:40:17 +0900

    20250119 PM 7:40

---
+++ minhojava/src/ch06/sec08/exam02/ComputerExample.java
@@ -0,0 +1,12 @@
+package ch06.sec08.exam02;
+
+public class ComputerExample {
+	public static void main(String[] args) {
+		//Computer 객체 생성
+		Computer myCom = new Computer();
+		
+		//sum() 메소드 호출 시 매개값 1, 2, 3을 제공하고
+		//합산 결과를 리턴받아 result1 변수에 대입
+		
+	}
+}
