d2ad4a9642aedb377b13530cada29fc8967c796e
rivermh <kmh000904@gmail.com>
2025-02-03 22:09:20 +0900

    20250203 PM 10:09

--- minhojava/src/ch07/sec10/exam02/AbstractMethodExample.java
+++ minhojava/src/ch07/sec10/exam02/AbstractMethodExample.java
@@ -11,7 +11,7 @@ public class AbstractMethodExample {
 		//매개변수의 다형성
 		animalSound(new Dog());
 		animalSound(new Cat());
-	}
+	} 
 	
 	public static void animalSound(Animal animal) {
 		animal.sound();
